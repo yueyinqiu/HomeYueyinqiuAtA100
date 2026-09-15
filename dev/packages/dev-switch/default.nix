1853e62d1574744292acb890b3058c51a38a9bce
yueyinqiu <yueyinqiu@outlook.com>
2026-09-15 20:29:31 +0800

    Update default.nix

--- dev/packages/dev-switch/default.nix
+++ dev/packages/dev-switch/default.nix
@@ -1,7 +1,7 @@
 { name, pkgs, nix-airgap, ... }:
 
 let
-  installable = ".#yueyinqiu@a100";
+  installable = ".#homeConfigurations.\"yueyinqiu@a100\"";
   remoteHost = "a100";
   remoteOutLink = "/media/a100/c5e1bf65-7974-432f-8aed-7a1345241efe/lujiaqi/yueyinqiu/.cache/dev-switch/result";
 in
